c59260205105ae412d7aa3390033c60896411765
Hacka-25 <oscarelijah09@gmail.com>
2025-08-06 07:57:20 +0300

    Initial commit - greeting program

---
+++ Favoritecolorgame.py
@@ -0,0 +1,8 @@
+#Ask the user for their name
+name = input("What is your name? ")
+
+#Ask the user for their favorite color
+color = input("What is your favoritr color? ")
+
+#Print a personalized greeting using the inputs
+print(f"Hello, {name}! Your favorite ccolor, {color}, is awesome!")
