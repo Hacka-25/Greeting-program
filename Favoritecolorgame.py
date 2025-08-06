dd04d4182467064489fb2c780f30f9f9eaf3c872
Hacka-25 <oscarelijah09@gmail.com>
2025-08-06 08:07:55 +0300

    Untrack wrong file

--- Favoritecolorgame.py
+++
@@ -1,8 +0,0 @@
-#Ask the user for their name
-name = input("What is your name? ")
-
-#Ask the user for their favorite color
-color = input("What is your favoritr color? ")
-
-#Print a personalized greeting using the inputs
-print(f"Hello, {name}! Your favorite ccolor, {color}, is awesome!")
